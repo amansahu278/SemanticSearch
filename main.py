1c1f1f5212af85f4698c31d3f7890eb113efde2a
amansahu278 <a.man.cubes24@gmail.com>
2025-04-30 02:33:20 -0400

    Added phonetic, fuzzy query expansion and corpus enrichment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 from asr.loader import load_asr
 from preprocessing.sentence_segmentation import segment_sentences
 from preprocessing.chunking import chunk_sentences
-from retrieval.retrievers import build_bm25_retriever, build_ensemble_retriever, build_deep_retriever
+from retrieval.retrievers import build_bm25_retriever, build_deep_retriever, _custom_bm25_preprocessing
 from utils.utils import extract_keywords, datetime_to_str
 from llm.summarizer import summarize
 
@@ -11,15 +11,11 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.docstore.in_memory import InMemoryDocstore
 
 from transformers import AutoTokenizer
-import tiktoken
-
+import phonetics
 from config import CHUNK_SIZE, CHUNK_OVERLAP
-# import sys, os
-# sys.path.append(os.path.abspath("/home/user/code/pwesuite"))
 
-# from models.metric_learning.model import RNNMetricLearner
-# from models.metric_learning.preprocessor import preprocess_dataset_foreign
-# from main.utils import load_multi_data
+from thefuzz import fuzz
+from thefuzz import process as fuzz_process
 
 import logging
 from logging import getLogger
@@ -30,6 +26,12 @@ def get_args():
     parser.add_argument("--filepath", type=str, required=True, help="Path to the ASR file")
     parser.add_argument("--query", type=str, help="Query for semantic search")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    parser.add_argument("--phonetic_enrichment", action="store_true", help="Enable phonetic enrichment for BM25")
+    parser.add_argument(
+        "--query_expansion", 
+        type=str,
+        choices=["phonetic", "fuzzy"], 
+        help="Enable query expansion for BM25. Options: 'phonetic' or 'fuzzy'.")
     return parser.parse_args()
 
 def load_and_process_asr(filepath, chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP):
@@ -41,9 +43,9 @@ def load_and_process_asr(filepath, chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OV
 
     return chunks
 
-def initialize_retrievers(chunks, idx_name="faiss_index", top_bm25=5, top_faiss=10):
+def initialize_retrievers(chunks, idx_name="faiss_index", top_bm25=5, top_faiss=10, use_phonetic_enrichment=False):
     # BM25 retriever initialization
-    bm25_retriever = build_bm25_retriever(chunks, k=top_bm25)
+    bm25_retriever = build_bm25_retriever(chunks, k=top_bm25, use_phonetic_enrichment=use_phonetic_enrichment)
 
     # FAISS retriever initialization
     embedding_model = HuggingFaceEmbeddings(model_name='intfloat/multilingual-e5-large-instruct')
@@ -51,7 +53,9 @@ def initialize_retrievers(chunks, idx_name="faiss_index", top_bm25=5, top_faiss=
 
     return bm25_retriever, faiss_retriever
 
-def retrieve_and_summarize(query, bm25_retriever, faiss_retriever, top_keywords=1):
+
+def retrieve_and_summarize_phonetic_enrichment(query, bm25_retriever, faiss_retriever, top_keywords=1):
+    logger.info(f"Retrieving with phonetic enrichment")
     # Load tokenizer 
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-instruct")
     # tokenizer = tiktoken.encoding_for_model("gpt-4")
@@ -60,28 +64,166 @@ def retrieve_and_summarize(query, bm25_retriever, faiss_retriever, top_keywords=
     keywords_list = extract_keywords(query, top=top_keywords)
     logger.debug(f"Keywords extracted: {keywords_list}")
 
-    # BM25 extraction
-    relevant_docs = []
-    for keywords in keywords_list:
-        retrieved_docs = bm25_retriever.invoke(keywords[0])
-        relevant_docs.extend(doc for doc in retrieved_docs)
-    logger.info(f"Number of documents: {len(relevant_docs)}")
-    # FAISS retrieval
-    retrieved_docs = faiss_retriever.invoke(query)
-    relevant_docs.extend(doc for doc in retrieved_docs)
+    # Find phonetic keys for the keywords
+    query_terms_for_bm25 = set()
+    for keyword in keywords_list:
+        key = phonetics.dmetaphone(keyword[0].strip().lower())
+        primary_key = key[0]
+        query_terms_for_bm25.add(primary_key)
+    
+    # Create the query string for BM25
+    final_query = " ".join(query_terms_for_bm25)
 
-    combined_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+    bm_25_relevant_docs = []
+    retrieved_docs = bm25_retriever.invoke(final_query)
+    for doc in retrieved_docs:
+        # Retain original text
+        doc.page_content = doc.metadata['original_text']
+        bm_25_relevant_docs.append(doc)
+    logger.info(f"Number of BM25 extracted documents: {len(bm_25_relevant_docs)}")
+
+    # FAISS retrieval
+    faiss_relevant_docs = faiss_retriever.invoke(query)
+    logger.info(f"Number of FAISS extracted documents: {len(faiss_relevant_docs)}")
+    
+    # Combine and sort documents
+    combined_docs = bm_25_relevant_docs + faiss_relevant_docs
+    combined_docs = sorted(combined_docs, key=lambda x: x.metadata.get('start_time', float('inf')))
+
+    # Create context
+    combined_text = "\n\n".join(doc.page_content for doc in combined_docs)
     logger.debug("Context is: %s", combined_text)
 
     num_tokens = len(tokenizer.encode(combined_text))
-    logger.info(f"Number of documents: {len(relevant_docs)}")
+    logger.info(f"Number of documents: {len(bm_25_relevant_docs) + len(faiss_relevant_docs)}")
     logger.info(f"Number of tokens in context: {num_tokens}")
 
     summary = summarize(combined_text, query)
 
     logger.info(f"\n📝 Answer:\n{summary}")
-    retrieved_docs.sort(key=lambda x: x.metadata['start_time'])
     for doc in retrieved_docs:
+        logger.info(f"{datetime_to_str(doc.metadata['start_time'])}: {datetime_to_str(doc.metadata['end_time'])}")
+
+def _query_expansion_preprocessing(chunks, create_phonetic_map=True):
+    tokenized_corpus_full = list(map(lambda x: _custom_bm25_preprocessing(x['text']), chunks))
+    corpus_vocab = list(set(token for doc_tokens in tokenized_corpus_full for token in doc_tokens))
+
+    phonetic_map = None
+    if create_phonetic_map:  
+        phonetic_map = {}  
+        for word in corpus_vocab:
+            try:
+                keys = phonetics.dmetaphone(word)
+                primary_key = keys[0]
+                if primary_key not in phonetic_map:
+                    phonetic_map[primary_key] = set()
+                phonetic_map[primary_key].add(word)
+            except IndexError: 
+                pass # Ignoring words that don't produce phonetic keys
+
+    return corpus_vocab, phonetic_map
+
+def retrieve_and_summarize_query_expansion(query, bm25_retriever, faiss_retriever, corpus_vocab=None, phonetic_map=None, fuzzy_score_cutoff=95, fuzzy_limit=5):
+    logger.info(f"Retrieving with query expansion, mode: {'phonetic' if phonetic_map else 'fuzzy'}")
+    # Load tokenizer 
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-instruct")
+    # tokenizer = tiktoken.encoding_for_model("gpt-4")
+
+    # Use keyword extraction for better input to the BM25 model. Look into Yake.
+    keywords_list = extract_keywords(query, top=1)
+    logger.debug(f"Keywords extracted: {keywords_list}")
+
+    phrase_tokens = _custom_bm25_preprocessing(keywords_list[0][0]) # call, of, duty
+
+    # TODO: Maybe add stop word removal for terms to expand
+    terms_to_expand = set(phrase_tokens) 
+    all_query_terms = set(phrase_tokens)
+
+    for term in terms_to_expand:
+        # Phonetic
+        if phonetic_map:
+            try:
+                key = phonetics.dmetaphone(term.lower())
+                primary_key = key[0]
+                if primary_key and primary_key in phonetic_map:
+                    variations = phonetic_map[primary_key] # call, caal, calll...
+                    all_query_terms.update(variations)
+            except IndexError:
+                pass
+        else:
+            # FUZZY
+            # --- ---
+            fuzzy_matches = fuzz_process.extract(term, corpus_vocab, scorer=fuzz.WRatio, limit=fuzzy_limit)
+            for match, score in fuzzy_matches:
+                logger.info(f"Term:{term}, Fuzzy match: {match}, Score: {score}")
+                if score >= fuzzy_score_cutoff:
+                    all_query_terms.add(match)
+            # --- ---
+
+    final_query = " ".join(all_query_terms)
+    logger.debug(f"Final query for BM25: {final_query}")
+
+    bm_25_relevant_docs = bm25_retriever.invoke(final_query)
+    logger.info(f"Number of BM25 extracted documents: {len(bm_25_relevant_docs)}")
+
+    # FAISS retrieval
+    faiss_relevant_docs = faiss_retriever.invoke(query)
+    logger.info(f"Number of FAISS extracted documents: {len(faiss_relevant_docs)}")
+
+    # Combine and sort documents
+    combined_docs = bm_25_relevant_docs + faiss_relevant_docs
+    combined_docs = sorted(combined_docs, key=lambda x: x.metadata.get('start_time', float('inf')))
+
+    # Create context
+    combined_text = "\n\n".join(doc.page_content for doc in combined_docs)
+    logger.debug("Context is: %s", combined_text)
+
+    num_tokens = len(tokenizer.encode(combined_text))
+    logger.info(f"Number of documents: {combined_docs}")
+    logger.info(f"Number of tokens in context: {num_tokens}")
+
+    summary = summarize(combined_text, query)
+
+    logger.info(f"\n📝 Answer:\n{summary}")
+    for doc in combined_docs:
+        logger.info(f"{datetime_to_str(doc.metadata['start_time'])}: {datetime_to_str(doc.metadata['end_time'])}")
+
+def retrieve_and_summarize_standard(query, bm25_retriever, faiss_retriever, top_keywords=1):
+    logger.info(f"Retrieving with standard BM25 and FAISS")
+    # Load tokenizer 
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-instruct")
+    # tokenizer = tiktoken.encoding_for_model("gpt-4")
+
+    # BM25 retrieval
+    # Use keyword extraction for better input to the BM25 model.
+    keywords_list = extract_keywords(query, top=top_keywords)
+    logger.debug(f"Keywords extracted: {keywords_list}")
+    
+    final_query = " ".join(map(lambda x: x[0].lower(), keywords_list))
+
+    bm_25_relevant_docs = bm25_retriever.invoke(final_query)
+    logger.info(f"Number of BM25 extracted documents: {len(bm_25_relevant_docs)}")
+
+    # FAISS retrieval
+    faiss_relevant_docs = faiss_retriever.invoke(query)
+    logger.info(f"Number of FAISS extracted documents: {len(faiss_relevant_docs)}")
+
+    # Combine and sort documents
+    combined_docs = bm_25_relevant_docs + faiss_relevant_docs
+    combined_docs = sorted(combined_docs, key=lambda x: x.metadata.get('start_time', float('inf')))
+
+    # Create context
+    combined_text = "\n\n".join(doc.page_content for doc in combined_docs)
+    logger.debug("Context is: %s", combined_text)
+
+    num_tokens = len(tokenizer.encode(combined_text))
+    logger.info(f"Number of documents: {combined_docs}")
+    logger.info(f"Number of tokens in context: {num_tokens}")
+
+    summary = summarize(combined_text, query)
+
+    logger.info(f"\n📝 Answer:\n{summary}")
+    for doc in combined_docs:
         logger.info(f"{datetime_to_str(doc.metadata['start_time'])}: {datetime_to_str(doc.metadata['end_time'])}")
 
 if __name__ == "__main__":
@@ -107,17 +249,31 @@ if __name__ == "__main__":
     chunks = load_and_process_asr(filepath)
     
     # Initialize retrievers
-    bm25_retriever, faiss_retriever = initialize_retrievers(chunks, idx_name=filepath.split('/')[-1].split('.')[0])
+    bm25_retriever, faiss_retriever = initialize_retrievers(chunks, idx_name=filepath.split('/')[-1].split('.')[0], use_phonetic_enrichment=args.phonetic_enrichment)
     
     # With the keyword extraction, better not to use the ensemble retriever
     # ensemble_retriever = build_ensemble_retriever(bm25_retriever, faiss_retriever)
 
     if query:
-        retrieve_and_summarize(query, bm25_retriever, faiss_retriever)
+        if args.phonetic_enrichment:
+            retrieve_and_summarize_phonetic_enrichment(query, bm25_retriever, faiss_retriever)
+        elif args.query_expansion:
+            expansion_type = args.query_expansion
+            corpus_vocab, phonetic_map = _query_expansion_preprocessing(chunks, create_phonetic_map= (expansion_type == "phonetic"))
+            retrieve_and_summarize_query_expansion(query, bm25_retriever, faiss_retriever, corpus_vocab=corpus_vocab, phonetic_map=phonetic_map)
+        else: 
+            retrieve_and_summarize_standard(query, bm25_retriever, faiss_retriever)
     else: 
         while True:
             query = input("Enter your query (or type 'exit' to quit): ").strip()
             if query.lower() == 'exit':
                 print("Exiting the program.")
                 break
-            retrieve_and_summarize(query, bm25_retriever, faiss_retriever)
+            if args.phonetic_enrichment:
+                retrieve_and_summarize_phonetic_enrichment(query, bm25_retriever, faiss_retriever)
+            elif args.query_expansion:
+                expansion_type = args.query_expansion
+                corpus_vocab, phonetic_map = _query_expansion_preprocessing(chunks, create_phonetic_map= (expansion_type == "phonetic"))
+                retrieve_and_summarize_query_expansion(query, bm25_retriever, faiss_retriever, corpus_vocab=corpus_vocab, phonetic_map=phonetic_map)
+            else: 
+                retrieve_and_summarize_standard(query, bm25_retriever, faiss_retriever)
--- retrieval/retrievers.py
+++ retrieval/retrievers.py
@@ -1,21 +1,60 @@
+import re
+from typing import List
 from langchain_community.retrievers import BM25Retriever
 from langchain.schema.document import Document
 from langchain.retrievers import EnsembleRetriever
-
 from langchain_community.vectorstores import FAISS
 
 from preprocessing.chunking import chunks_to_docs
+import phonetics
 
 from logging import getLogger
 import os
 logger = getLogger(__name__)
 
-def build_bm25_retriever(chunks, k=5):
-  logger.info("Initializing BM25 retriever")
-  docs = chunks_to_docs(chunks)
-  retriever = BM25Retriever.from_documents(docs)
-  retriever.k = k
-  return retriever
+def _custom_bm25_preprocessing(text: str) -> List[str]:
+    """
+    Custom preprocessing function for BM25 tokenization.
+    """
+    bm25_tokenization_regex = r'(?u)\b\w+\b'
+    text = text.lower()
+    tokenizer = re.compile(bm25_tokenization_regex).findall
+    # tokens = re.findall(bm25_tokenization_regex, text)
+    return tokenizer(text)
+
+def _enrich_text_with_phonetics(text):
+    tokens = _custom_bm25_preprocessing(text)
+    enriched_tokens = set(tokens)
+    for token in tokens:
+        try:
+            keys = phonetics.dmetaphone(token)
+            primary_key = keys[0]
+            if primary_key:
+                enriched_tokens.add(primary_key)
+        except IndexError:
+            pass
+    return " ".join(list(enriched_tokens))
+
+def build_bm25_retriever(chunks, k=5, use_phonetic_enrichment=False):
+    logger.info("Initializing BM25 retriever")
+    docs = chunks_to_docs(chunks)
+
+    if use_phonetic_enrichment:
+        logger.info("Using phonetics for BM25 tokenization")
+        enriched_docs = []
+        for doc in docs:
+            metadata = doc.metadata
+            metadata['original_text'] = doc.page_content
+            enriched_doc = Document(
+                page_content=_enrich_text_with_phonetics(doc.page_content),
+                metadata=doc.metadata
+            )
+            enriched_docs.append(enriched_doc)
+        docs = enriched_docs
+        
+    retriever = BM25Retriever.from_documents(docs)
+    retriever.k = k
+    return retriever
 
 def build_deep_retriever(chunks, vector_store, embeddings, k=5, index_name=None):
     logger.info("Initializing FAISS retriever")
@@ -25,7 +64,7 @@ def build_deep_retriever(chunks, vector_store, embeddings, k=5, index_name=None)
         index_path = os.path.join("faiss_index", index_name)
         if os.path.exists(index_path):
             logger.info(f"Loading FAISS index from {index_path}")
-            vectordb = vector_store.load_local(index_path, embeddings)
+            vectordb = vector_store.load_local(index_path, embeddings, allow_dangerous_deserialization=True)
         else:
             logger.info(f"Creating and saving FAISS index to {index_path}")
             vectordb = vector_store.from_documents(docs, embeddings)
